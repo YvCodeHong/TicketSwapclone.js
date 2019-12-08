97566f4fb4bdb017c2d1de2af37c20437b18d4f1
Yvonne Tang <ytang604@gmail.com>
2019-12-08 00:04:14 +0000

    take speech marks off

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -5,7 +5,7 @@ class SearchBar extends React.Component {
     render() {
         return (
             <div className="SearchBar">
-                "The safest way to buy and sell e-tickets"
+                The safest way to buy and sell e-tickets
                 <input placeholder="Search for events, venues and cities"/>
                 <button className="SearchButton">SEARCH</button>
             </div>
